6c091ba3b1673986b8f82f1855dd83dd03dc8567
Remko de Jong <remko.de.jong@jungleminds.nl>
2019-01-13 13:58:19 +0100

    updated routing setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router";
 
 import ShowPage from "./pagetypes/ShowPage";
 import EpisodePage from "./pagetypes/EpisodePage";
 
 class App extends Component {
-  // componentDidMount() {}
-
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={ShowPage} />
-          <Route path="/episode/:season-:number" component={EpisodePage} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/" component={ShowPage} />
+        <Route exact path="/episode-:season-:number" component={EpisodePage} />
+      </Switch>
     );
   }
 }
